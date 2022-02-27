9e82b5079c451e55996674b30d36c93f5063a00f
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-27 21:16:21 +0600

    add details function and add some stye for make fancy

--- js/app.js
+++ js/app.js
@@ -24,3 +24,25 @@ const loadAllPics = (pics) => {
     parent.appendChild(div);
   }
 };
+
+const loadDetails = (id) => {
+  fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+    .then((res) => res.json())
+    .then((data) => details(data));
+};
+const details = (thisPic) => {
+  console.log(thisPic);
+  const parentDetails = document.getElementById("detailsConainers");
+  parentDetails.textContent = "";
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="relative">
+    <img src="${thisPic.url}" class="height w-100" alt="">
+    <img src="${thisPic.thumbnailUrl}" class="rounded-circle shadow profile" alt="">
+  </div>
+  <div class="mt-5">
+      <h4 class="pt-3 mb-4">${thisPic.title}</h4>
+  </div>
+  `;
+  parentDetails.appendChild(div);
+};
